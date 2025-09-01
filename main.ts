efb99bf01d04b34cfb6ad5f38f3a82bdbdd984b3
DwayneDG <dwayne.pp29@gmail.com>
2025-09-01 14:10:08 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    CutebotPro.fullSpeedAhead()
+    basic.pause(5000)
+})
+CutebotPro.colorLight(CutebotProRGBLight.RGBL, 0xff0000)
+CutebotPro.colorLight(CutebotProRGBLight.RGBR, 0xff0000)
